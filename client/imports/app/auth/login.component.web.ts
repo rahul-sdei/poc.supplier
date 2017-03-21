134e029165db356b96828cbbf16aeacb93b36fea
Mihir Jain <mihir.jain@smartdatainc.net>
2017-03-21 14:28:27 +0530

    added checkvalue from remember me

--- client/imports/app/auth/login.component.web.ts
+++ client/imports/app/auth/login.component.web.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, NgZone} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Accounts } from 'meteor/accounts-base';
 import { Meteor } from 'meteor/meteor';
 import {MeteorComponent} from 'angular2-meteor';
 import template from './login.component.web.html';
@@ -14,7 +15,8 @@ import {validateEmail, validatePhoneNum, validateFirstName} from "../../validato
 export class LoginComponent extends MeteorComponent implements OnInit {
     loginForm: FormGroup;
     error: string;
-
+    isAvailable = false;
+    userId: string;
     constructor(private router: Router, private zone: NgZone, private formBuilder: FormBuilder) {
       super();
     }
@@ -29,19 +31,22 @@ export class LoginComponent extends MeteorComponent implements OnInit {
     }
 
     login() {
-          if (this.loginForm.valid) {
-              Meteor.loginWithPassword(this.loginForm.value.email, this.loginForm.value.password, (err) => {
-                  if (err) {
-                      this.zone.run(() => {
-                        this.error = err;
-                      });
-                  } else {
-                      showAlert("You have been logged in successfully.", "success");
-                      this.router.navigate(['/dashboard']);
-                  }
-              });
+            if (this.loginForm.valid) {
+                Meteor.loginWithPassword(this.loginForm.value.email, this.loginForm.value.password, (err) => {
+                    if (err) {
+                        this.zone.run(() => {
+                          this.error = err;
+                        });
+                    } else {
+                        Accounts.config({
+                          loginExpirationInDays: 30
+                        });
+                        showAlert("You have been logged in successfully.", "success");
+                        this.router.navigate(['/dashboard']);
+                    }
+                });
+            }
           }
-      }
 
     fblogin(): void {
      Meteor.loginWithFacebook({requestPermissions: ['public_profile,email']}, (err) => {
