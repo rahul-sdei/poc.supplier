88e03c11e5cbf6bcdf0766a7ffe743395ec26cf6
Rahul Sethi <rahulse.sdei@gmail.com>
2017-03-28 17:56:04 +0530

    added charts in dashboard summary

--- client/imports/app/dashboard/dashboard.component.ts
+++ client/imports/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewChecked, NgZone } from '@angular/core';
+import { Component, OnInit, AfterViewInit, AfterViewChecked, NgZone } from '@angular/core';
 import { Meteor } from "meteor/meteor";
 import { InjectUser } from "angular2-meteor-accounts-ui";
 import { Router, ActivatedRoute } from '@angular/router';
@@ -7,6 +7,8 @@ import template from "./dashboard.html";
 import { Booking } from "../../../../both/models/booking.model";
 import { Observable, Subscription, Subject, BehaviorSubject } from "rxjs";
 import { ChangeDetectorRef } from "@angular/core";
+import { Chart } from 'chart.js';
+import * as _ from 'underscore';
 import { showAlert } from "../shared/show-alert";
 
 interface Pagination {
@@ -23,7 +25,7 @@ interface Options extends Pagination {
   template
 })
 @InjectUser('user')
-export class DashboardComponent extends MeteorComponent implements OnInit, AfterViewChecked {
+export class DashboardComponent extends MeteorComponent implements OnInit, AfterViewInit, AfterViewChecked {
   userId: string;
   items: Booking[];
 
@@ -33,6 +35,122 @@ export class DashboardComponent extends MeteorComponent implements OnInit, After
       private changeDetectorRef: ChangeDetectorRef,
   ) {
       super();
+  }
+
+  ngAfterViewInit() {
+    let ctx = document.getElementById("myChart");
+    let myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Week 1", "Week 2", "Week 3", "Week 4", "Week 5", "Week 6"],
+            datasets: [{
+                label: 'Bookings',
+                data: [12, 19, 13, 16, 14, 18],
+                backgroundColor: [
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+
+    let ctx2 = document.getElementById("myChart2");
+    let myChart2 = new Chart(ctx2, {
+        type: 'bar',
+        data: {
+            labels: ["Week 1", "Week 2", "Week 3", "Week 4", "Week 5", "Week 6"],
+            datasets: [{
+                label: 'Revenue',
+                data: [1750, 1550, 1950, 1450, 1850, 2050],
+                backgroundColor: [
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+
+    let ctx3 = document.getElementById("myChart3");
+    let myChart3 = new Chart(ctx3, {
+        type: 'bar',
+        data: {
+            labels: ["Week 1", "Week 2", "Week 3", "Week 4", "Week 5", "Week 6"],
+            datasets: [{
+                label: 'Views',
+                data: [125, 135, 115, 145, 105, 155],
+                backgroundColor: [
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
   }
 
   ngAfterViewChecked() {
