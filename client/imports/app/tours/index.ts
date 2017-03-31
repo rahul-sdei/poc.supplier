c70ba3eae3e7b538d2b04982addec391eb1dd607
Mihir Jain <mihir.jain@smartdatainc.net>
2017-03-31 10:21:01 +0530

    add tour design and functionality step 1 integration

--- client/imports/app/tours/index.ts
+++ client/imports/app/tours/index.ts
@@ -1,4 +1,6 @@
 import { ListPageComponent } from './list';
+import { CreateComponent } from './step1';
 export const Tour_Declarations = [
-    ListPageComponent
+    ListPageComponent,
+    CreateComponent
 ];
--- client/imports/app/tours/routes.ts
+++ client/imports/app/tours/routes.ts
@@ -2,7 +2,8 @@ import { Route } from '@angular/router';
 import { Meteor } from 'meteor/meteor';
 
 import { ListPageComponent } from './list';
-
+import { CreateComponent } from './step1';
 export const routes = [
-    {path: "tours/list", component: ListPageComponent }
+    {path: "tours/list", component: ListPageComponent },
+    {path: "tours/create/step1", component: CreateComponent }
 ];
