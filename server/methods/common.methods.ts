b9f920ae63b08b58436a8d2a6cc7d17c9309da34
Mihir Jain <mihir.jain@smartdatainc.net>
2017-05-24 11:47:44 +0530

    changes in mailgun email function

--- server/methods/common.methods.ts
+++ server/methods/common.methods.ts
@@ -10,11 +10,18 @@ Meteor.methods({
       return Email.send({ to, from, subject, html});
   },
   "sendEmailCustom": (to: string, subject: string, text: string) => {
-      let Mailgun = require('mailgun').Mailgun;
-      let email = new Mailgun('key-755f427d33296fe30862b0278c460e84');
-      let domain = "sandbox3f697e79ae2849f5935a5a60e59f9795.mailgun.org";
+      let mailgunKey = Meteor.settings.public["mailgun"] ["key"];
+      let mailgunDomain = Meteor.settings.public["mailgun"] ["domain"];
+      let mailgun = require('mailgun-js')({apiKey: mailgunKey, domain: mailgunDomain});
 
-      email.sendText("noreply@atorvia.com", to, subject, text, domain, (err) => {
+      let data = {
+        from: "noreply@atorvia.com",
+        to: to,
+        subject: subject,
+        html: text
+      }
+
+      mailgun.messages().send(data, (err) => {
         if (err) {
           console.log(err);
         }
